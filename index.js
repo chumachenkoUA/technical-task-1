e2cc709be4929a56522269d9894ed1a9d1845d15
cumachenko <kirito22122005@gmail.com>
2025-02-12 21:17:20 +0200

    Додав функціонал до кнопки

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Happy developing ✨')
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('first-name').value;
+    const surname = document.getElementById('last-name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const userData = {
+        name: name,
+        surname: surname,
+        email: email,
+        password: password
+    }
+    submit(userData);
+})
+
+function submit(userData) {
+    console.log(userData);
+}
